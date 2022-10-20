2bd8b918ef3cb28702fa91d25dc34f31ec771bcb
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-10-20 18:28:27 +0200

    struggle with rendering

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,6 +1,70 @@
-import React, { Component } from "react";
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+const host = "http://localhost:3000/";
 
-function AddTask() {
-    return <div>AddTask</div>;
+function AddTask(props) {
+    const [taskName, setTaskName] = useState("Task title");
+    const [showNewTask, setShowNewTask] = useState("visible");
+    const [showAddTask, setShowAddTask] = useState("hidden");
+    async function postTask(projectId) {
+        let id = new Date();
+        id = Math.round(id.getTime() * (Math.random() * 666));
+        try {
+            const response = await axios.post(`${host}tasks`, {
+                id: id,
+                projectId: projectId,
+                title: taskName,
+            });
+            props.getData("projects");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div>
+            <button
+                className={showNewTask}
+                id="add-task-button"
+                onClick={() => {
+                    setShowAddTask("visible");
+                    setShowNewTask("hidden");
+                }}
+            >
+                New task
+            </button>
+
+            <div id="task-input" className={showAddTask}>
+                <b>Title</b><br />
+                <input
+                    type="text"
+                    onChange={(e) => {
+                        setTaskName(e.target.value);
+                    }}
+                    onFocus={() => {
+                        setTaskName("");
+                    }}
+                    value={taskName}
+                />
+                <br />
+                <button
+                    onClick={() => {
+                        postTask(props.projectId);
+                    }}
+                >
+                    Add task
+                </button>
+                <button
+                    onClick={() => {
+                        setShowAddTask("hidden");
+                        setShowNewTask("visible");
+                        setTaskName("Task name");
+                    }}
+                >
+                    Abort
+                </button>
+            </div>
+        </div>
+    );
 }
 export default AddTask;
--- frontend/src/pages/Overview.jsx
+++ frontend/src/pages/Overview.jsx
@@ -1,14 +1,20 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, Children } from "react";
 import AddProject from "../components/AddProject";
 import AddTask from "../components/AddTask";
+import AddTask2 from "../components/AddTask2";
 import Navbar from "../components/Navbar";
 import { useProjectContext } from "../context/ProjectContext";
+import axios from "axios";
+const host = "http://localhost:3000/";
 
 function Overview() {
     const providerValue = useProjectContext();
     const [projects, setProjects] = useState([]);
     const [tasks, setTasks] = useState([]);
     const [timelogs, setTimelogs] = useState([]);
+    const [showHideProjects, setShowHideProjects] = useState("visible");
+    const [showHideTasks, setShowHideTasks] = useState("hidden");
+    const [showHideTimelogs, setShowHideTimelogs] = useState("hidden");
 
     async function getData(type) {
         if (type === "projects") {
@@ -24,37 +30,60 @@ function Overview() {
             setTimelogs(data);
         }
     }
-/*
-    useEffect(() => {
-        console.log("projects");
-        console.log(projects);
-    }, [projects]);
-    
-    useEffect(() => {
-        console.log("tasks");
-        console.log(tasks);
-    }, [tasks]);
 
+    async function deleteObject(element, id) {
+        try {
+            const response = await axios.delete(`${host}${element}/${id}`);
+            await getData(element);
+        } catch (error) {
+            //setErrorMsg(`${error.response.data.status}: ${error.response.data.message}`);
+            console.log(error);
+        }
+    }
+    function view(element) {
+        if (element === "projects") {
+            setShowHideProjects("visible");
+            setShowHideTasks("hidden");
+            setShowHideTimelogs("hidden");
+        }
+        if (element === "tasks") {
+            setShowHideProjects("hidden");
+            setShowHideTasks("visible");
+            setShowHideTimelogs("hidden");
+        }
+        if (element === "timelogs") {
+            setShowHideProjects("hidden");
+            setShowHideTasks("hidden");
+            setShowHideTimelogs("visible");
+        }
+    }
     useEffect(() => {
-        console.log("timelogs");
-        console.log(timelogs);
-    }, [timelogs]);
-*/
+        getData("projects");
+    }, [showHideProjects]);
     return (
         <div>
-            <h1><a href="/overview">Overview</a></h1>
-            <AddProject id="add-project" />
-            <AddTask id="add-task" />
+            <h1>
+                <a href="/overview">Overview</a>
+            </h1>
+            <div className={showHideProjects}>
+                <AddProject getData={getData} id="add-project" />
+            </div>
+            {/*<div className={showHideTasks}>
+                <AddTask2 hidden={"hidden"} getData={getData} id="add-project" />
+    </div>*/}
             <button
                 onClick={() => {
                     getData("projects");
+                    view("projects");
                 }}
             >
-                Projekt
+                Projects
             </button>
             <button
                 onClick={() => {
                     getData("tasks");
+                    getData("projects");
+                    view("tasks");
                 }}
             >
                 Tasks
@@ -62,38 +91,80 @@ function Overview() {
             <button
                 onClick={() => {
                     getData("timelogs");
+                    view("timelogs");
                 }}
             >
                 Timelogs
             </button>
-            {projects.map((project) => {
-                return (
-                    <div key={`proj_${project.id}`} className="project-container">
-                        <div>Id: {project.id}</div>
-                        <div>Name: {project.name}</div>
-                        <div>Color: {project.color}</div>
-                    </div>
-                );
-            })}
-            {tasks.map((task) => {
-                return (
-                    <div key={`task_${task.id}`} className="project-container task">
-                        <div>Id: {task.id}</div>
-                        <div>ProjId: {task.projectId}</div>
-                        <div>Title: {task.title}</div>
-                    </div>
-                );
-            })}
-            {timelogs.map((timelog) => {
-                return (
-                    <div key={`time_${timelog.id}`} className="project-container time">
-                        <div>Id: {timelog.id}</div>
-                        <div>TaskId: {timelog.taskId}</div>
-                        <div>Start: {timelog.start}</div>
-                        <div>End: {timelog.end}</div>
-                    </div>
-                );
-            })}
+            <div className={showHideProjects}>
+                {projects.map((project) => {
+                    return (
+                        <div
+                            key={`proj_${project.id}`}
+                            className="project-container"
+                            style={{ background: project.color }}
+                        >
+                            <button
+                                onClick={() => {
+                                    deleteObject("projects", project.id);
+                                }}
+                            >
+                                x
+                            </button>
+                            <div>Id: {project.id}</div>
+                            <div>Name: {project.name}</div>
+                            <div>Color: {project.color}</div>
+                            <AddTask getData={getData} projectId={project.id} id="add-task" />
+                        </div>
+                    );
+                })}
+            </div>
+            <div className={showHideTasks}>
+                {tasks.map((task) => {
+                    let color;
+                    let hej;
+                    projects.map((project) => {
+                        project.id === task.projectId ? (color = project.color) : (hej = null);
+                    });
+                    return (
+                        <div
+                            key={`task_${task.id}`}
+                            className="project-container task"
+                            style={{ background: color }}
+                        >
+                            <div>Id: {task.id}</div>
+                            <div>ProjId: {task.projectId}</div>
+                            <div>Title: {task.title}</div>
+                            <button
+                                onClick={() => {
+                                    deleteObject("tasks", task.id);
+                                }}
+                            >
+                                x
+                            </button>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className={showHideTimelogs}>
+                {timelogs.map((timelog) => {
+                    return (
+                        <div key={`time_${timelog.id}`} className="project-container time">
+                            <div>Id: {timelog.id}</div>
+                            <div>TaskId: {timelog.taskId}</div>
+                            <div>Start: {timelog.start}</div>
+                            <div>End: {timelog.end}</div>
+                            <button
+                                onClick={() => {
+                                    deleteObject("timelogs", timelog.id);
+                                }}
+                            >
+                                x
+                            </button>
+                        </div>
+                    );
+                })}
+            </div>
             <Navbar />
         </div>
     );
